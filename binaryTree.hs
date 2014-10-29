cfb1a51206cf7ad0ad68a876847b5ee816ac74f9
Tim Wilkens <timwilkensf1@gmail.com>
2014-10-29 09:05:25 -0700

    Update functions to be foldable

--- binaryTree.hs
+++ binaryTree.hs
@@ -1,21 +1,20 @@
 data Tree a = Node (Tree a) a (Tree a)
             | Empty
-  deriving (Show, Read, Eq)
+  deriving (Show, Eq)
+
+leaf :: Ord a => a -> Tree a
+leaf x = Node Empty x Empty
 
 treeFromList :: Ord a => [a] -> Tree a
 treeFromList [] = Empty
-treeFromList (x:xs) = treeFromList' (Node Empty x Empty) xs
+treeFromList (x:xs) = treeFromList' (leaf x) xs
 
 treeFromList' :: Ord a => Tree a -> [a] -> Tree a
 treeFromList' t [] = t
 treeFromList' t (x:xs) = treeFromList' (addItem x t) xs
 
-treeToList :: Ord a => Tree a -> [a]
-treeToList Empty = []
-treeToList (Node left x right) = (treeToList left) ++ [x] ++ (treeToList right)
-
 addItem :: Ord a => a -> Tree a -> Tree a
-addItem x Empty = Node Empty x Empty
+addItem x Empty = leaf x
 addItem x t@(Node left y right)
   |y < x = Node left y (addItem x right)
   |y > x = Node (addItem x left) y right
@@ -27,6 +26,22 @@ treeElem x (Node left y right)
   | x == y = True
   | x < y = treeElem x left
   | x > y = treeElem x right
+
+treeToList :: Tree a -> [a]
+treeToList Empty = []
+treeToList (Node left x right) = (treeToList left) ++ [x] ++ (treeToList right)
+
+numberOfNodes :: Tree a -> Integer
+numberOfNodes Empty = 0
+numberOfNodes (Node l _ r) = 1 + (numberOfNodes l) + (numberOfNodes r)
+
+treeSum :: Tree Integer -> Integer
+treeSum Empty = 0
+treeSum (Node l x r) = x + (treeSum l) + (treeSum r)
+
+treeDepth :: Tree a -> Integer
+treeDepth Empty = 0
+treeDepth (Node l _ r) = 1 + (treeDepth l) + (treeDepth r)
 
 minItem :: Tree a -> Maybe a
 minItem Empty = Nothing
@@ -45,10 +60,3 @@ sameTree (Node l1 x1 r1) (Node l2 x2 r2)
   | otherwise = False
 sameTree _ _ = False
 
-height :: Tree a -> Int
-height t = height' 0 t
-
-height' :: Int -> Tree a -> Int
-height' x Empty = x
-height' x (Node left _ right) =
-  max (height' (x + 1) left) (height' (x + 1) right)
