e81e638565b1d7c8fbc475e285237f5cfe139108
Tim Wilkens <timwilkensf1@gmail.com>
2014-10-30 08:17:21 -0700

    Add treeFold

--- binaryTree.hs
+++ binaryTree.hs
@@ -27,21 +27,42 @@ treeElem x (Node left y right)
   | x < y = treeElem x left
   | x > y = treeElem x right
 
+-- Arguments passed in are
+-- 1. Return value for Empty
+-- 2. Fold function that makes recursive calls on subtrees and current node value
+-- 3. The tree itself
+
+treeFold :: b -> (b -> a -> b -> b) -> Tree a -> b
+treeFold e _ Empty = e
+treeFold e f (Node l x r) = f (treeFold e f l) x (treeFold e f r)
+
 treeToList :: Tree a -> [a]
 treeToList Empty = []
 treeToList (Node left x right) = (treeToList left) ++ [x] ++ (treeToList right)
+
+treeToList' :: Tree a -> [a]
+treeToList' = treeFold [] (\l x r -> l ++ [x] ++ r)
 
 numberOfNodes :: Tree a -> Integer
 numberOfNodes Empty = 0
 numberOfNodes (Node l _ r) = 1 + (numberOfNodes l) + (numberOfNodes r)
 
+numberOfNodes' :: Tree a -> Integer
+numberOfNodes' = treeFold 0 (\l _ r -> l + 1 + r)
+
 treeSum :: Tree Integer -> Integer
 treeSum Empty = 0
 treeSum (Node l x r) = x + (treeSum l) + (treeSum r)
 
+treeSum' :: Tree Integer -> Integer
+treeSum' = treeFold 0 (\l x r -> l + x + r)
+
 treeDepth :: Tree a -> Integer
 treeDepth Empty = 0
-treeDepth (Node l _ r) = 1 + (treeDepth l) + (treeDepth r)
+treeDepth (Node l _ r) = 1 + max (treeDepth l) (treeDepth r)
+
+treeDepth' :: Tree a -> Integer
+treeDepth' = treeFold 0 (\l _ r -> 1 + max l r)
 
 minItem :: Tree a -> Maybe a
 minItem Empty = Nothing
