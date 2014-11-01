82026a4c27576be912a5aeb7ba0790d09865b2bb
Tim Wilkens <timwilkensf1@gmail.com>
2014-10-31 18:41:23 -0700

    Add fold version of min and max

--- binaryTree.hs
+++ binaryTree.hs
@@ -27,52 +27,27 @@ treeElem x (Node left y right)
   | x < y = treeElem x left
   | x > y = treeElem x right
 
--- Arguments passed in are
--- 1. Return value for Empty
--- 2. Fold function that makes recursive calls on subtrees and current node value
--- 3. The tree itself
-
 treeFold :: b -> (b -> a -> b -> b) -> Tree a -> b
 treeFold e _ Empty = e
 treeFold e f (Node l x r) = f (treeFold e f l) x (treeFold e f r)
 
-treeToList :: Tree a -> [a]
-treeToList Empty = []
-treeToList (Node left x right) = (treeToList left) ++ [x] ++ (treeToList right)
-
 treeToList' :: Tree a -> [a]
 treeToList' = treeFold [] (\l x r -> l ++ [x] ++ r)
-
-numberOfNodes :: Tree a -> Integer
-numberOfNodes Empty = 0
-numberOfNodes (Node l _ r) = 1 + (numberOfNodes l) + (numberOfNodes r)
 
 numberOfNodes' :: Tree a -> Integer
 numberOfNodes' = treeFold 0 (\l _ r -> l + 1 + r)
 
-treeSum :: Tree Integer -> Integer
-treeSum Empty = 0
-treeSum (Node l x r) = x + (treeSum l) + (treeSum r)
-
 treeSum' :: Tree Integer -> Integer
 treeSum' = treeFold 0 (\l x r -> l + x + r)
-
-treeDepth :: Tree a -> Integer
-treeDepth Empty = 0
-treeDepth (Node l _ r) = 1 + max (treeDepth l) (treeDepth r)
 
 treeDepth' :: Tree a -> Integer
 treeDepth' = treeFold 0 (\l _ r -> 1 + max l r)
 
-minItem :: Tree a -> Maybe a
-minItem Empty = Nothing
-minItem (Node Empty x _) = Just x
-minItem (Node left x _) = minItem left
+treeMin :: (Ord a, Bounded a) => Tree a -> a
+treeMin = treeFold maxBound (\l x r -> l `min` x `min` r)
 
-maxItem :: Tree a -> Maybe a
-maxItem Empty = Nothing
-maxItem (Node _ x Empty) = Just x
-maxItem (Node _ x right) = maxItem right
+treeMax :: (Ord a, Bounded a) => Tree a -> a
+treeMax = treeFold minBound (\l x r -> l `max` x `max` r)
 
 sameTree :: Eq a => Tree a -> Tree a -> Bool
 sameTree Empty Empty = True
