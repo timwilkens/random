cddf70bf1741cc77b4845ed53befccea246372e0
Tim Wilkens <timwilkensf1@gmail.com>
2015-01-10 17:20:19 -0800

    Update to use Data.Sequence instead of List
    Allow user to specify max depth from command line. Still not working
    but getting closer.

--- rubiks.hs
+++ rubiks.hs
@@ -1,3 +1,7 @@
+import qualified Data.Sequence as S
+import Data.Foldable (toList)
+import System.Environment
+
 data Color = White|Green|Orange|Yellow|Red|Blue
   deriving (Eq)
 
@@ -12,12 +16,12 @@ instance Show Color where
   show Red    = "\x1b[31mR" ++ colorOff
   show Blue   = "\x1b[34mB" ++ colorOff
 
-data Cube = Cube { front :: [Color]
-                 , back :: [Color]
-                 , left :: [Color]
-                 , right :: [Color]
-                 , top :: [Color]
-                 , bottom :: [Color]
+data Cube = Cube { front :: S.Seq Color
+                 , back :: S.Seq Color
+                 , left :: S.Seq Color
+                 , right :: S.Seq Color
+                 , top :: S.Seq Color
+                 , bottom :: S.Seq Color
                  }
   deriving (Show, Eq)
 
@@ -32,23 +36,25 @@ instance Show Move where
 cubeListOrder :: [String]
 cubeListOrder = ["front", "back", "left", "right", "top", "bottom"]
 
-cubeToList :: Cube -> [[Color]]
-cubeToList c = [front c, back c, left c, right c, top c, bottom c]
+cubeToSeq :: Cube -> [S.Seq Color]
+cubeToSeq c = [front c, back c, left c, right c, top c, bottom c]
 
 isSolved :: Cube -> Bool
-isSolved c = isSolved' $ cubeToList c
-  where isSolved' xs = foldr (&&) True (map solvedSide xs)
+isSolved c = isSolved' $ cubeToSeq c
+--  where isSolved' xs = all (==True) (map solvedSide xs)
+  where isSolved' xs = any (==True) (map solvedSide xs)
 
-solvedSide :: [Color] -> Bool
-solvedSide [] = True
-solvedSide (x:xs) = foldr (&&) True (map (== x) xs)
+solvedSide :: S.Seq Color -> Bool
+solvedSide xs = all (==True) (map (== h) end)
+  where h = S.index xs 0
+        end = toList $ S.drop 1 xs
 
 cubeString :: Cube -> String
-cubeString c = foldr (++) "" (map entitle $ zip cubeListOrder (cubeToList c))
+cubeString c = foldr (++) "" (map entitle $ zip cubeListOrder (cubeToSeq c))
   where entitle (a,b) = "[" ++ a ++ "]\n" ++ (sideAsString b)
 
-sideAsString :: [Color] -> String
-sideAsString s = asString s 1
+sideAsString :: S.Seq Color -> String
+sideAsString s = asString (toList s) 1
   where asString [] n = ""
         asString (x:[]) n = show x ++ "\n"
         asString (x:xs) n = 
@@ -64,40 +70,49 @@ applyMoves [] c = c
 applyMoves (x:xs) c = applyMoves xs (applyMove x c)
 
 moves :: [Move]
-moves = [rotateFrontRight, rotateZAxisRight, rotateLeftBack, rotateYAxisBack,
-         rotateTopCounterC, rotateYAxisForward, rotateTopClockwise, rotateLeftForward,
-         rotateXAxisRight, rotateFrontLeft, rotateXAxisLeft, rotateZAxisLeft]
+moves = [rotateFrontRight, rotateZAxisRight, rotateYAxisForward,
+         rotateTopClockwise, rotateLeftForward, rotateXAxisRight]
 
 base :: [[[Move]]]
 base = [addMoves []]
 
 addMovesOfNPlusOne :: [[[Move]]] -> [[[Move]]]
-addMovesOfNPlusOne (x:xs) = (buildMoves x):(x:xs)
+addMovesOfNPlusOne (x:xs) = (buildMoves x):((filter (\y -> isSolved (applyMoves y cube)) x):xs)
+--addMovesOfNPlusOne (x:xs) = (buildMoves x):(x:xs)
 addMovesOfNPlusOne [] = base
 
 addMoves :: [Move] -> [[Move]]
-addMoves m = filter noMoveCycle $ filter noMoveUndo $ map (\x -> m ++ [x]) moves
+addMoves m = filter noRepeats $ map (\x -> m ++ [x]) moves
 
 buildMoves :: [[Move]] -> [[Move]]
 buildMoves m = concatMap addMoves m
 
-buildAll :: [[[Move]]]
-buildAll = reverse $ buildAll' base 1
-  where buildAll' xs 25 = xs
-        buildAll' xs n = buildAll' (addMovesOfNPlusOne xs) (n + 1)
+buildAll :: Int -> [[[Move]]]
+-- Add reverse at the start to do breadth first search
+-- Without we start from leaves and contract
+buildAll max = reverse $ buildAll' base 1 max
+  where buildAll' xs n max =
+          if n == max
+            then xs
+            else buildAll' (addMovesOfNPlusOne xs) (n + 1) max
+
+noRepeats :: [Move] -> Bool
+noRepeats x = noMoveUndo x' && noMoveCycle x'
+  where x' = reverse x
 
 noMoveCycle :: [Move] -> Bool
-noMoveCycle xs = noMoveCycle' (reverse xs)
-  where noMoveCycle' (a:b:c:d:_) = 
-          if (a == b) && (b == c) && (c == d)
-            then False
-            else True
-        noMoveCycle' _ = True
+noMoveCycle (a:b:c:d:_) = 
+  if (a == b) && (b == c) && (c == d)
+    then False
+    else True
+noMoveCycle _ = True
 
 noMoveUndo :: [Move] -> Bool
-noMoveUndo xs = noMoveUndo' (reverse xs)
-  where noMoveUndo' (x:y:_) = if x == (opposite y) then False else True
-        noMoveUndo' _ = True
+noMoveUndo (x:y:_) = 
+  if x == (opposite y)
+    then False
+    else True
+noMoveUndo _ = True
 
 opposite :: Move -> Move
 opposite (Move m s) = case s of
@@ -122,10 +137,10 @@ rotateFrontLeft = Move frontLeft "Rotate front face to the left"
 frontRight :: Cube -> Cube
 frontRight c = Cube { front = permuteFaceRight (front c)
                     , back = back c
-                    , left = [(l!!0),(l!!1),(b!!0),(l!!3),(l!!4),(b!!1),(l!!6),(l!!7),(b!!2)]
-                    , right = [(t!!6),(r!!1),(r!!2),(t!!7),(r!!4),(r!!5),(t!!8),(r!!7),(r!!8)]
-                    , top = (take 6 t) ++ [(l!!8),(l!!5),(l!!2)]
-                    , bottom = [(r!!6),(r!!3),(r!!0)] ++ drop 3 b 
+                    , left = S.update 8 (S.index b 2) (S.update 5 (S.index b 1) (S.update 2 (S.index b 0) l))
+                    , right = S.update 6 (S.index t 8) (S.update 3 (S.index t 7) (S.update 0 (S.index t 6) r))
+                    , top = S.update 8 (S.index l 2) (S.update 7 (S.index l 5) (S.update 6 (S.index l 8) t))
+                    , bottom = S.update 2 (S.index r 0) (S.update 1 (S.index r 3) (S.update 0 (S.index r 6) b))
                     }
   where b = bottom c
         t = top c
@@ -135,7 +150,10 @@ frontRight c = Cube { front = permuteFaceRight (front c)
 frontLeft :: Cube -> Cube
 frontLeft = frontRight . frontRight . frontRight
 
-permuteFaceRight (a:b:c:d:e:f:g:h:i:[]) = [g, d, a, h, e, b, i, f, c]
+
+-- Expensive
+permuteFaceRight xs = permuteFaceRight' (toList xs)
+  where permuteFaceRight' (a:b:c:d:e:f:g:h:i:[]) = S.fromList [g, d, a, h, e, b, i, f, c]
 
 -----------------------------
 
@@ -143,12 +161,12 @@ rotateLeftBack = Move leftBackward "Rotate left side backward"
 rotateLeftForward = Move leftForward "Rotate left side forward"
 
 leftForward :: Cube -> Cube
-leftForward c = Cube { front = [(t!!0),(f!!1),(f!!2),(t!!3),(f!!4),(f!!5),(t!!6),(f!!7),(f!!8)]
-                     , back = [(ba!!0),(ba!!1),(b!!6),(ba!!3),(ba!!4),(b!!3),(ba!!6),(ba!!7),(b!!0)] 
+leftForward c = Cube { front = S.update 6 (S.index t 6) (S.update 3 (S.index t 3) (S.update 0 (S.index t 0) f))
+                     , back = S.update 8 (S.index b 0) (S.update 5 (S.index b 3) (S.update 2 (S.index b 6) ba))
                      , left = permuteFaceRight (left c)
                      , right = right c
-                     , top = [(ba!!8),(t!!1),(t!!2),(ba!!5),(t!!4),(t!!5),(ba!!2),(t!!7),(t!!8)] 
-                     , bottom = [(f!!0),(b!!1),(b!!2),(f!!3),(b!!4),(b!!5),(f!!6),(b!!7),(b!!8)]
+                     , top = S.update 6 (S.index ba 2) (S.update 3 (S.index ba 5) (S.update 0 (S.index ba 8) t))
+                     , bottom = S.update 6 (S.index f 6) (S.update 3 (S.index f 3) (S.update 0 (S.index f 0) b))
                      }
   where f = front c
         b = bottom c
@@ -163,10 +181,10 @@ rotateTopClockwise = Move topC "Rotate top clockwise"
 rotateTopCounterC = Move topCC "Rotate top counter clockwise"
 
 topC :: Cube -> Cube
-topC c = Cube { front = [(r!!0),(r!!1),(r!!2)] ++ drop 3 f
-              , back = [(l!!0),(l!!1),(l!!2)] ++ drop 3 ba
-              , left = [(f!!0),(f!!1),(f!!2)] ++ drop 3 l
-              , right = [(ba!!0),(ba!!1),(ba!!2)] ++ drop 3 r
+topC c = Cube { front = (S.take 3 r) S.>< (S.drop 3 f)
+              , back = (S.take 3 l) S.>< (S.drop 3 ba)
+              , left = (S.take 3 f) S.>< (S.drop 3 l)
+              , right = (S.take 3 ba) S.>< (S.drop 3 r)
               , top = permuteFaceRight (top c)
               , bottom = bottom c
               }
@@ -183,12 +201,12 @@ rotateYAxisBack = Move yAxisBackward "Rotate vertical middle back"
 rotateYAxisForward = Move yAxisForward "Rotate vertical middle forward"
 
 yAxisBackward :: Cube -> Cube
-yAxisBackward c = Cube { front = [(f!!0),(b!!1),(f!!2),(f!!3),(b!!4),(f!!5),(f!!6),(b!!7),(f!!8)]
-                       , back = [(ba!!0),(t!!7),(ba!!2),(ba!!3),(t!!4),(ba!!5),(ba!!6),(t!!1),(ba!!8)] 
+yAxisBackward c = Cube { front = S.update 7 (S.index b 7) (S.update 4 (S.index b 4) (S.update 1 (S.index b 1) f))
+                       , back = S.update 7 (S.index t 1) (S.update 4 (S.index t 4) (S.update 1 (S.index t 7) ba))
                        , left = left c
                        , right = right c
-                       , top = [(t!!0),(f!!1),(t!!2),(t!!3),(f!!4),(t!!5),(t!!6),(f!!7),(t!!8)]
-                       , bottom = [(b!!0),(ba!!7),(b!!2),(b!!3),(ba!!4),(b!!5),(b!!6),(ba!!1),(b!!8)]
+                       , top = S.update 7 (S.index f 7) (S.update 4 (S.index f 4) (S.update 1 (S.index f 1) t))
+                       , bottom = S.update 7 (S.index ba 1) (S.update 4 (S.index ba 4) (S.update 1 (S.index ba 7) b))
                        }
   where f = front c
         t = top c
@@ -203,10 +221,10 @@ rotateXAxisRight = Move xAxisRight "Rotate horizontal middle right"
 rotateXAxisLeft = Move xAxisLeft "Rotate horizontal middle left"
 
 xAxisRight :: Cube -> Cube
-xAxisRight c = Cube { front = take 3 f ++ [(l!!3),(l!!4),(l!!5)] ++ drop 6 f
-                    , back = take 3 ba ++ [(r!!3),(r!!4),(r!!5)] ++ drop 6 ba
-                    , left = take 3 l ++ [(ba!!3),(ba!!4),(ba!!5)] ++ drop 6 l
-                    , right = take 3 r ++ [(f!!3),(f!!4),(f!!5)] ++ drop 6 r
+xAxisRight c = Cube { front = S.update 5 (S.index l 5) (S.update 4 (S.index l 4) (S.update 3 (S.index l 3) f))
+                    , back = S.update 5 (S.index r 5) (S.update 4 (S.index r 4) (S.update 3 (S.index r 3) ba))
+                    , left = S.update 5 (S.index ba 5) (S.update 4 (S.index ba 4) (S.update 3 (S.index ba 3) l))
+                    , right = S.update 5 (S.index f 5) (S.update 4 (S.index f 4) (S.update 3 (S.index f 3) r))
                     , top = top c
                     , bottom = bottom c
                     }
@@ -225,10 +243,10 @@ rotateZAxisLeft = Move zAxisLeft "Rotate z axis left"
 zAxisRight :: Cube -> Cube
 zAxisRight c = Cube { front = front c
                     , back = back c
-                    , left = [(l!!0),(b!!3),(l!!2),(l!!3),(b!!4),(l!!5),(l!!6),(b!!5),(l!!8)] 
-                    , right = [(r!!0),(t!!3),(r!!2),(r!!3),(t!!4),(r!!5),(r!!6),(t!!5),(r!!8)]
-                    , top = take 3 t ++ [(l!!7),(l!!4),(l!!1)] ++ drop 6 t
-                    , bottom = take 3 b ++ [(r!!7),(r!!4),(r!!1)] ++ drop 6 b
+                    , left = S.update 7 (S.index b 5) (S.update 4 (S.index b 4) (S.update 1 (S.index b 3) l))
+                    , right = S.update 7 (S.index t 5) (S.update 4 (S.index t 4) (S.update 1 (S.index t 3) r))
+                    , top = S.update 5 (S.index l 1) (S.update 4 (S.index l 4) (S.update 3 (S.index l 7) t))
+                    , bottom = S.update 5 (S.index r 1) (S.update 4 (S.index r 4) (S.update 3 (S.index r 7) b))
                     }
   where t = top c
         l = left c
@@ -240,12 +258,12 @@ zAxisLeft = zAxisRight . zAxisRight . zAxisRight
 -----------------------------
 
 -- Front has green center
-fronts  = [Green, Green, Blue, Orange, Green, Red, Yellow, White, Yellow]
-backs   = [Red, Orange, Red, Blue, Blue, Red, Orange, Blue, Green]
-lefts   = [Yellow, Red, Orange, Blue, Orange, Green, Red, Green, Green]
-rights  = [White, Yellow, White, Yellow, Red, Yellow, Blue, Blue, White]
-bottoms = [Orange, Green, Orange, Red, Yellow, Orange, Yellow, White, Blue]
-tops    = [Blue, White, White, White, White, Orange, White, Yellow, Red]
+fronts  = S.fromList [Green, Green, Blue, Orange, Green, Red, Yellow, White, Yellow]
+backs   = S.fromList [Red, Orange, Red, Blue, Blue, Red, Orange, Blue, Green]
+lefts   = S.fromList [Yellow, Red, Orange, Blue, Orange, Green, Red, Green, Green]
+rights  = S.fromList [White, Yellow, White, Yellow, Red, Yellow, Blue, Blue, White]
+bottoms = S.fromList [Orange, Green, Orange, Red, Yellow, Orange, Yellow, White, Blue]
+tops    = S.fromList [Blue, White, White, White, White, Orange, White, Yellow, Red]
 
 cube = Cube { front = fronts
             , back = backs
@@ -255,12 +273,14 @@ cube = Cube { front = fronts
             , top = tops
             }
 
-solutions :: [[[Move]]]
-solutions = map checkForSolutions $ drop 15 buildAll
+solutions :: Int -> [[[Move]]]
+solutions max = map checkForSolutions $ buildAll max
 
 checkForSolutions :: [[Move]] -> [[Move]]
 checkForSolutions xs =  filter (\x -> isSolved (applyMoves x cube)) xs
 
 main = do
-  putStrLn "Starting up"
-  putStrLn $ show solutions
+  args <- getArgs
+  let maxDepth = (head args)
+  putStrLn $ "Setting max depth to " ++ show (head args)
+  putStrLn $ show $ buildAll (read maxDepth)
