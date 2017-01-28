92bdba88ca3d403e23113de747f5027d737ec3d5
timwilkens <timw@mindtouch.com>
2017-01-28 14:47:42 -0800

    Add retry logic

--- hackernews/app/Main.hs
+++ hackernews/app/Main.hs
@@ -62,7 +62,7 @@ instance FromJSON HNItem where
   parseJSON Null = pure EmptyItem
 
 instance Show HNItem where
-  show EmptyItem = "No response"
+  show EmptyItem = "Empty Response"
   show h = idString ++ " " ++ case itemType h of
     Nothing -> "Unknown item type"
     Just t -> case t of
@@ -74,31 +74,31 @@ instance Show HNItem where
 -- Why is this necessary?
 instance Exception BL.ByteString where
 
-data HNError = FailedToGetMaxItem HttpException
-             | InvalidMaxItemResponse BL.ByteString
-             | FailedToGetItemInfo HttpException
+data HNError = MaxItemFailure HttpException
+             | MalformedMaxItem BL.ByteString
+             | MalformedItemInfo
+             | ItemInfoFailure HttpException
   deriving (Show)
 
 getItemInfo :: Integer -> IO (Either HNError (String, String))
-getItemInfo id = do
-  now <- getCurrentTime
-  (process now <$> (get (itemInfoUrl id) >>= asJSON)) `E.catch` handler
+getItemInfo id =
+  (process <$> (get (itemInfoUrl id) >>= asJSON)) `E.catch` handler
     where
-      process now item =
+      process item =
         let body = item ^. responseBody in
           case body of
-            EmptyItem -> Right (show body, getPrettyTime now)
+            EmptyItem -> Left MalformedItemInfo
             _         -> Right (show body, getPrettyTimeFromItemTimeStamp $ time body)
-      handler = return . Left . FailedToGetItemInfo
+      handler = return . Left . ItemInfoFailure
 
 getMaxItemId :: IO (Either HNError Integer)
 getMaxItemId = (process <$> get maxHackerNewsIdUrl) `E.catch` handler
   where
     process response =
       case Char8.readInteger (response ^. responseBody) of
-          Nothing       -> Left (InvalidMaxItemResponse $ response ^. responseBody)
-          Just (n,rest) -> Right n
-    handler = return . Left . InvalidMaxItemResponse
+        Nothing       -> Left (MalformedMaxItem $ response ^. responseBody)
+        Just (n,rest) -> Right n
+    handler = return . Left . MaxItemFailure
 
 getPrettyTime :: UTCTime -> String
 getPrettyTime = formatTime defaultTimeLocale "%c"
@@ -106,51 +106,49 @@ getPrettyTime = formatTime defaultTimeLocale "%c"
 getPrettyTimeFromItemTimeStamp :: Maybe Integer -> String
 getPrettyTimeFromItemTimeStamp t = getPrettyTime $ posixSecondsToUTCTime $ realToFrac (fromMaybe 0 t)
 
-fetchItemChunk :: Integer -> Integer -> IO Integer
-fetchItemChunk low high =
-  sum <$> forM [low..high] (\x -> do
-    r <- getItemInfo x
-    case r of
-      Left e -> do
-        print e
-        return 1 :: IO Integer
-      Right (info, prettyTime) -> do
-        P.putStrLn $ prettyTime ++ " " ++ info
-        P.putStrLn ""
-        return 0)
+fetchItemChunk :: [Integer] -> IO Integer
+fetchItemChunk ids = fetchItemChunk' ids 0
+  where
+    fetchItemChunk' [] failures = return failures
+    fetchItemChunk' ids failures
+      | failures >= maxFailures = return failures
+      | otherwise =
+        forM ids (\x -> do
+          r <- getItemInfo x
+          case r of
+            Left e -> return (1,[x])
+            Right (info, prettyTime) -> do
+              P.putStrLn $ prettyTime ++ " " ++ info
+              P.putStrLn ""
+              return (0, [])) >>= \ts ->
+                fetchItemChunk' (P.concatMap snd ts) (sum $ P.map fst ts)
 
 fetchLoop :: Integer -> Integer -> IO ()
-fetchLoop previousId numFailures =
-  if numFailures >= maxFailures
-    then P.putStrLn "Number of consecutive failures exceeded"
-    else do
+fetchLoop previousId failures
+  | failures >= maxFailures =
+      P.putStrLn "Number of consecutive failures exceeded"
+  | otherwise = do
       delay (microPerSecond * 3)
       maxItemId <- getMaxItemId
       case maxItemId of
-        Left e -> do
-          print e
-          fetchLoop previousId (numFailures + 1)
-        Right id ->
-          if id == previousId
-            then fetchLoop previousId 0
-            else do
+        Left e -> fetchLoop previousId (failures + 1)
+        Right id
+          | id == previousId -> fetchLoop previousId 0
+          | otherwise -> do
               delay microPerSecond
-              failures <- fetchItemChunk (previousId+1) id
-              fetchLoop id failures
+              fetchItemChunk [(previousId+1)..id] >> fetchLoop id 0
 
-main' :: Integer -> IO ()
-main' failures =
-  if failures >= maxFailures
-    then P.putStrLn "Max failures exceeded getting max item id"
-    else do
+mainLoop :: Integer -> IO ()
+mainLoop failures
+  | failures >= maxFailures =
+      P.putStrLn "Max failures exceeded getting max item id"
+  | otherwise = do
       maxItemId <- getMaxItemId
       case maxItemId of
-        Left e   -> do
-          print e
-          main' (failures + 1)
+        Left e   -> mainLoop (failures + 1)
         Right id -> fetchLoop (id-1) 0
 
 main :: IO ()
 main = do
   P.putStrLn "Starting up..."
-  main' 0
+  mainLoop 0
